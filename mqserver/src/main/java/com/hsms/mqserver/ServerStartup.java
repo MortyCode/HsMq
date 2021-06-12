2ae0c539ce64ee95ee317cf8b1d9255bdeb86477
海鹏 <lhp0305@vip.163.com>
2021-06-12 16:45:35 +0800

    \n

--- mqserver/src/main/java/com/hsms/mqserver/ServerStartup.java
+++ mqserver/src/main/java/com/hsms/mqserver/ServerStartup.java
@@ -15,6 +15,16 @@ public class ServerStartup {
 
     public static void start(String[] args){
         try {
+            String start =
+                    " _   _   _____       ___  ___   _____    \n" +
+                    "| | | | /  ___/     /   |/   | /  _  \\   \n" +
+                    "| |_| | | |___     / /|   /| | | | | |   \n" +
+                    "|  _  | \\___  \\   / / |__/ | | | | | |   \n" +
+                    "| | | |  ___| |  / /       | | | |_| |_  \n" +
+                    "|_| |_| /_____/ /_/        |_| \\_______|  \n"+
+                    "v 1.0.0 河神大人 \n";
+            System.out.println(start);
+
             ObjectReactor baseServer = new ObjectReactor(9001);
             baseServer.start();
         } catch (InterruptedException e) {
